b3dbbe91d66b1dde0f766e9c7d04b214edbe3bc8
YAN NICOLAS CASAGRANDE <yan.casagrande@univille.br>
2024-09-10 19:52:33 -0300

    first commit

---
+++ src/main/java/br/univille/fsoweb20242yan/Fsoweb20242yanApplication.java
@@ -0,0 +1,13 @@
+package br.univille.fsoweb20242yan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fsoweb20242yanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fsoweb20242yanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/univille/fsoweb20242yan/Fsoweb20242yanApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.fsoweb20242yan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fsoweb20242yanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
